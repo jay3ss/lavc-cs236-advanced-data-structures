fe5f9086b399d47b6096b7590053c0b0da6a1f69
jay ess <mentoce@gmail.com>
2020-02-11 11:12:49 -0800

    Implement methods

--- assignments/assignment01/TicTacToe.cpp
+++ assignments/assignment01/TicTacToe.cpp
@@ -1,2 +1,134 @@
 #include "TicTacToe.h"
 
+// Default constructor
+ticTacToe::ticTacToe()
+{
+    restart();
+}
+
+// Prints the board
+void ticTacToe::displayBoard() const
+{
+    const int numCellsAndRows = 3;
+
+    cout << "1   2   3\n\n";
+
+    for (int i = 0; i < numCellsAndRows; i++)
+    {
+        cout << i + 1;
+
+        for (int j = 0; j < numCellsAndRows; j++)
+        {
+            cout << "  " << board[i][j] << (j < 2 ? "|" : "\n");
+
+        }
+
+        cout << (i < 2 ? "____|___|___\n" : "    |   |\n");
+    }
+}
+
+// Determines the current status of the game
+status ticTacToe::gameStatus()
+{
+    const int numCellsAndRows = 3;
+
+    // if the number of moves is equal to the maximum number of possible
+    // moves, then the game is at a draw
+    if (noOfMoves == maxNoOfMoves)
+    {
+        return DRAW;
+    }
+
+    // check if any rows, columns, or diagonals are the same
+    // if they are, then the game is won
+    for (int i = 0; i < numCellsAndRows; i++)
+    {
+        if (isEntireRowXOrO(i) || isEntireColumnXOrO(i))
+        {
+            return WIN;
+        }
+    }
+
+    // Diagonals
+    if (areDiagonalsAllXOrO())
+    {
+        return WIN;
+    }
+
+    // Otherwise, the game can continue
+    return CONTINUE;
+}
+
+// Gets the move for a player
+bool ticTacToe::getXOMove(const char moveSymbol)
+{
+    // 'X' goes on even numbered turns while 'O' goes on odd ones
+    bool isXsMove = noOfMoves % 2 == 0;
+    return (isXsMove && moveSymbol == 'X') || (!isXsMove && moveSymbol == 'O');
+}
+
+bool ticTacToe::isValidMove(const int x, const int y) const
+{
+    return (board[x][y] != 'X') || (board[x][y] != 'O');
+}
+
+// Restarts the game
+void ticTacToe::restart()
+{
+    initializeBoard();
+    noOfMoves = 0;
+}
+
+// Initializes the board to have all empty cells (' ' character)
+void ticTacToe::initializeBoard()
+{
+    const int numCellsAndRows = 3;
+
+    for (int i = 0; i < numCellsAndRows; i++)
+    {
+        for (int j = 0; j < numCellsAndRows; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+// Determines if an entire row is an 'X' or a 'O'
+bool ticTacToe::isEntireColumnSame(const int col) const
+{
+    return (board[col][0] == board[col][1]) && (board[col][1] == board[col][2]);
+}
+
+// Determines if an entire column has the same character in each cell
+bool ticTacToe::isEntireColumnXOrO(const int col) const
+{
+    bool colIsSame = isEntireColumnSame(col);
+    return (colIsSame && (board[col][0] == 'X' || board[col][0] == 'O'));
+}
+
+// Determines if an entire row is an 'X' or a 'O'
+bool ticTacToe::isEntireRowSame(const int row) const
+{
+    return (board[0][row] == board[1][row]) && (board[1][row] == board[2][row]);
+}
+
+// Determines if an entire column has the same character in each cell
+bool ticTacToe::isEntireRowXOrO(const int row) const
+{
+    bool rowIsSame = isEntireRowSame(row);
+    return (rowIsSame && (board[0][row] == 'X' || board[0][row] == 'O'));
+}
+
+// Determines if either of the diagonals is all 'X's or 'O's
+bool ticTacToe::areDiagonalsSame() const
+{
+    bool firstDiag = board[0][0] == board[1][1] && board[1][1] == board[2][2];
+    bool secondDiag = board[0][2] == board[1][1] && board[1][1] == board[2][0];
+    return firstDiag || secondDiag;
+}
+
+// Determines if either of the diagonals is all 'X's or 'O's
+bool ticTacToe::areDiagonalsAllXOrO() const
+{
+    return areDiagonalsSame() && (board[1][1] == 'X' || board[1][1] == 'O');
+}
--- assignments/assignment01/TicTacToe.h
+++ assignments/assignment01/TicTacToe.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 
 using namespace std;
@@ -7,16 +8,19 @@ class ticTacToe
 {
 public:
     // Default constructor
-    // Postcondition: initializes the board to an empty state;
+    // Postcondition: initializes the board to an empty state
+    ticTacToe();
 
     // Starts the game
     void play();
 
     // Prints the board
+    // Precondition: the board must have been initialized
     void displayBoard() const;
 
     // Determines if a move is valid
-    bool isValidMove(int x, int y) const;
+    // Precondition: the board must have been initialized
+    bool isValidMove(const int x, const int y) const;
 
     // Gets the move for a player
     bool getXOMove(char moveSymbol);
@@ -30,6 +34,26 @@ public:
 private:
     char board[3][3];
     int noOfMoves;
+    const int maxNoOfMoves = 9; // corresponds to a draw
 
+    // Initializes the board to have all empty cells (' ' character)
     void initializeBoard();
+
+    // Determines if an entire column has the same character in each cell
+    bool isEntireColumnSame(const int col) const;
+
+    // Determines if an entire column is an 'X' or a 'O'
+    bool isEntireColumnXOrO(const int col) const;
+
+    // Determines if an entire row has the same character in each cell
+    bool isEntireRowSame(const int row) const;
+
+    // Determines if an entire row is an 'X' or a 'O'
+    bool isEntireRowXOrO(const int row) const;
+
+    // Determines if either diagonal has the same character in each cell
+    bool areDiagonalsSame() const;
+
+    // Determines if either of the diagonals is all 'X's or 'O's
+    bool areDiagonalsAllXOrO() const;
 };
