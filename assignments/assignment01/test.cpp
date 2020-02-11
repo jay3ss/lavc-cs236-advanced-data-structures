6240ca7e7a88a660a4a3cc754f68481ec0530e96
jay ess <mentoce@gmail.com>
2020-02-11 11:12:20 -0800

    Add test driver

--- assignments/assignment01/test.cpp
+++ assignments/assignment01/test.cpp
@@ -1,6 +1,53 @@
+#include <cassert>
+#include <iostream>
 #include "TicTacToe.h"
+
+using namespace std;
+
+void displayStatus(ticTacToe &);
 
 int main()
 {
 
+    ticTacToe game;
+
+    // When starting the game:
+    // - its status should be CONTINUE
+    assert(game.gameStatus() == CONTINUE);
+    displayStatus(game);
+
+    // - every move should be a valid move
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            assert(game.isValidMove(i, j));
+        }
+    }
+
+    // - 'X' starts the game
+    assert(game.getXOMove('X'));
+
+    // - 'O' doesn't start the game
+    assert(!game.getXOMove('O'));
+
+    game.displayBoard();
+}
+
+void displayStatus(ticTacToe &game)
+{
+    cout << "The current game status is ";
+
+    switch (game.gameStatus())
+    {
+    case WIN:
+        cout << "WIN\n";
+        break;
+    case DRAW:
+        cout << "DRAW\n";
+        break;
+    default:
+        cout << "CONTINUE\n";
+        break;
+    }
 }
