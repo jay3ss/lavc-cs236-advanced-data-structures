c1cf66b757411cd312773d16d19d5c5f0bc80561
jay ess <mentoce@gmail.com>
2020-02-29 20:05:38 -0800

    Put more spaces between output of tests

--- assignments/assignment02/tests.cpp
+++ assignments/assignment02/tests.cpp
@@ -12,7 +12,7 @@ int main()
     arrList.print();
     cout << endl;
 
-    cout << "Insert data into the empty list\n";
+    cout << "\nInsert data into the empty list\n";
     for (int i = 0; i < 26; i++)
     {
         arrList.insertAt(i, i*i);
@@ -22,42 +22,42 @@ int main()
     arrList.print();
     cout << endl;
 
-    cout << "Inserting data into the middle of a list that has data\n";
+    cout << "\nInserting data into the middle of a list that has data\n";
     arrList.insertAt(17, 1000);
 
     cout << "Printing the data in the list: ";
     arrList.print();
     cout << endl;
 
-    cout << "Inserting data at the end of the list that has data\n";
+    cout << "\nInserting data at the end of the list that has data\n";
     arrList.insertAt(27, 54321);
 
     cout << "Printing the data in the list: ";
     arrList.print();
     cout << endl;
 
-    cout << "Inserting data at the beginning of the list that has data\n";
+    cout << "\nInserting data at the beginning of the list that has data\n";
     arrList.insertAt(0, 314159);
 
     cout << "Printing the data in the list: ";
     arrList.print();
     cout << endl;
 
-    cout << "Inserting data that is out of range (negative location: -10)\n";
+    cout << "\nInserting data that is out of range (negative location: -10)\n";
     arrList.insertAt(-10, 54321);
 
     cout << "Printing the data in the list: ";
     arrList.print();
     cout << endl;
 
-    cout << "Inserting data that is out of range (beyond max size: 10000)\n";
+    cout << "\nInserting data that is out of range (beyond max size: 10000)\n";
     arrList.insertAt(10000, 54321);
 
     cout << "Printing the data in the list: ";
     arrList.print();
     cout << endl;
 
-    cout << "Fill the list and then insert more data:\n";
+    cout << "\nFill the list and then insert more data:\n";
     for (int i = 29; i < maxListSize; i++)
     {
         arrList.insertAt(i, i*i);
@@ -67,7 +67,7 @@ int main()
     arrList.print();
     cout << endl;
 
-    cout << "Insert data beyond max size of list (50):\n";
+    cout << "\nInsert data into a full list:\n";
     arrList.insertAt(51, 17);
 
     return 0;
