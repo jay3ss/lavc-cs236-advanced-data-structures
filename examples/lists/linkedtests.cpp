da82c1e9d41582a27def0bf9e9665e06e0bd8723
jay ess <mentoce@gmail.com>
2020-03-06 11:44:14 -0800

    Initial tests for the linked list class

--- examples/lists/linkedtests.cpp
+++ examples/lists/linkedtests.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     const int LENGTH = 10;
-    LinkedList<int> list(LENGTH);
+    LinkedList<int> list;
 
     // The list's current position should start at 0
     assert(list.currPos() == 0);
@@ -16,7 +16,7 @@ int main()
         list.append(i * i - i);
     }
 
-    // The list's current position should stil be at 0
+    // The list's current position should still be at 0
     assert(list.currPos() == 0);
 
     // Calling next should move the list's current position to 1
