56b59f1c44f9edda8c6e2a63711d485251b9b438
jay ess <mentoce@gmail.com>
2020-03-05 17:42:15 -0800

    Implement method to clear and reset the list

--- examples/lists/ArrayList.cpp
+++ examples/lists/ArrayList.cpp
@@ -10,3 +10,14 @@ ArrayList<E>::~ArrayList()
     delete[] listArray;
     listArray = nullptr;
 }
+
+template <class E>
+void ArrayList<E>::clear()
+{
+    // Delete the list and create a new one and set the list size and current
+    // position to zero
+    delete[] listArray;
+    listArray = new E[maxSize];
+    listSize = 0;
+    curr = 0;
+}
