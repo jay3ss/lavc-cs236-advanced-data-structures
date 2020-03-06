5264f9cc222b5c8b4e3b36e4b1ee718fec02c601
jay ess <mentoce@gmail.com>
2020-03-06 11:43:59 -0800

    Initial definition of a linked list

--- examples/lists/LinkedList.h
+++ examples/lists/LinkedList.h
@@ -2,7 +2,58 @@
 #define LINKED_LIST_H
 
 #include "ListInterface.h"
+#include "Node.h"
 
+template <class E>
+class LinkedList : public ListInterface<E>
+{
+private:
+    Node<E>* head;
+public:
+    LinkedList() {}  // Default constructor
+    ~LinkedList() {} // Base destructor
 
+    // Clear contents from the list, to make it empty
+    void clear();
+
+    // Insert an element at the current location
+    // item: The element to be inserted
+    void insert(const E &item);
+
+    // Append an element at the end of the list
+    // item: The element to be inserted
+    void append(const E &item);
+
+    // Remove and return the current element
+    // Return: The element that was removed
+    E remove();
+
+    // Set the current position to the start of the list
+    void moveToStart();
+
+    // Set the current position to the end of the list
+    void moveToEnd();
+
+    // Move the current position one step left. No change if
+    // already at the beginning
+    void prev();
+
+    // Move the current position one step right. No change if
+    // already at the end
+    void next();
+
+    // Return: The number of elements in the list
+    int length() const;
+
+    // Return the position of the current element
+    int currPos() const;
+
+    // Set the current position
+    // pos: The position to make current
+    void moveToPos(int pos);
+
+    // Return: The current element
+    const E &getValue() const;
+};
 
 #endif // LINKED_LIST_H
