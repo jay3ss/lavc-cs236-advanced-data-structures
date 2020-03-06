1e8d69536285ed68d10f9246c4f4e5dedf6e57cf
jay ess <mentoce@gmail.com>
2020-03-05 17:35:00 -0800

    Implement array list destructor

--- examples/lists/ArrayList.cpp
+++ examples/lists/ArrayList.cpp
@@ -3,3 +3,10 @@ ArrayList<E>::ArrayList(int size) : maxSize(size), listSize(0), curr(0)
 {
     listArray = new E[maxSize];
 }
+
+template <class E>
+ArrayList<E>::~ArrayList()
+{
+    delete listArray;
+    listArray = nullptr;
+}
