72de13788a247ffebe7900c2461bc2af110167cb
jay ess <mentoce@gmail.com>
2020-03-16 16:01:36 -0700

    Add comments

--- examples/stacks/ArrayStack.cpp
+++ examples/stacks/ArrayStack.cpp
@@ -1,11 +1,13 @@
 #include "ArrayStack.h"
 
+// Constructor
 template<class T>
 ArrayStack<T>::ArrayStack(const int maxNumItems) : maxSize(maxNumItems), top(-1)
 {
     items = new T[maxSize];
 }
 
+// Destructor
 template<class T>
 ArrayStack<T>::~ArrayStack()
 {
