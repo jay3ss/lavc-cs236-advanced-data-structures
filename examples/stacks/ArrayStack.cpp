e5d3640ebda138c407da86dd7ced75744aa5d982
jay ess <mentoce@gmail.com>
2020-03-16 16:04:01 -0700

    Implement method to check if the stack is empty

--- examples/stacks/ArrayStack.cpp
+++ examples/stacks/ArrayStack.cpp
@@ -14,3 +14,10 @@ ArrayStack<T>::~ArrayStack()
     delete[] items;
     items = nullptr;
 }
+
+// Determines if the stack is empty
+template <class T>
+bool ArrayStack<T>::isEmpty() const
+{
+    return top == -1;
+}
