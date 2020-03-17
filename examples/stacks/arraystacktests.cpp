af3a132e95a0b373ceac604b4cd610c0c14e988e
jay ess <mentoce@gmail.com>
2020-03-16 17:23:06 -0700

    Add test for emptiness of empty stack

--- examples/stacks/arraystacktests.cpp
+++ examples/stacks/arraystacktests.cpp
@@ -9,5 +9,8 @@ int main()
     assert(stack.length() == 0);
     std::cout << "An empty stack should have no items in it: PASSED\n\n";
 
+    assert(stack.isEmpty());
+    std::cout << "An empty stack should be empty: PASSED\n\n";
+
     return 0;
 }
