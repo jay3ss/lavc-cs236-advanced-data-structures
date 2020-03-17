4a3e6961c4de2486120aa9580d955ad103bb5980
jay ess <mentoce@gmail.com>
2020-03-17 15:10:59 -0700

    Define method stubs

---
+++ examples/stacks/LinkedStack.cpp
@@ -0,0 +1,62 @@
+#include "LinkedStack.h"
+
+template <class T>
+LinkedStack<T>::LinkedStack(const int maxNumItems) : topPtr(nullptr)
+{
+
+}
+
+template <class T>
+LinkedStack<T>::~LinkedStack()
+{
+
+}
+
+// Determines if the stack is empty
+template <class T>
+bool LinkedStack<T>::isEmpty() const
+{
+    return false;
+}
+
+// Determines if the stack is full
+template <class T>
+bool LinkedStack<T>::isFull() const
+{
+    return false;
+}
+
+// Removes the top item from the stack
+template <class T>
+bool LinkedStack<T>::pop()
+{
+    return false;
+}
+
+// Adds an item to the top of the stack
+template <class T>
+bool LinkedStack<T>::push(const T& item)
+{
+    return false;
+}
+
+// Returns the top item from the stack without modifying the stack
+template <class T>
+const T& LinkedStack<T>::top() const
+{
+    return topPtr->data();
+}
+
+// Returns the number of items currently in the stack
+template <class T>
+const int LinkedStack<T>::length() const
+{
+    return -1;
+}
+
+// Returns the stack to its initial state - empty
+template <class T>
+void LinkedStack<T>::clear()
+{
+
+}
