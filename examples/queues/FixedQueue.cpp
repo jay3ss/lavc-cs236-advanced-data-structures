5b54d386111dc2490d11b641455689b104a0c759
jay ess <mentoce@gmail.com>
2020-03-18 13:41:26 -0700

    Implement remaining methods

--- examples/queues/FixedQueue.cpp
+++ examples/queues/FixedQueue.cpp
@@ -20,21 +20,42 @@ FixedQueue<T>::~FixedQueue()
 template <class T>
 bool FixedQueue<T>::isEmpty() const
 {
-    return false;
+    return rear == 0;
 }
 
 // Adds a new entry to the back of the queue
 template <class T>
 bool FixedQueue<T>::enqueue(const T& newEntry)
 {
-    return false;
+    bool canEnqueue = !isFull();
+
+    if (canEnqueue)
+    {
+        queue[rear] = newEntry;
+        rear++;
+    }
+
+    return canEnqueue;
 }
 
 // Removes the entry at the front of the queue
 template <class T>
 bool FixedQueue<T>::dequeue()
 {
-    return false;
+    bool canDequeue = !isEmpty();
+
+    if (canDequeue)
+    {
+        // move every entry after the front of the queue
+        // up one position
+        for (int i = rear; i < 0; i--)
+        {
+            queue[rear - 1] = queue[rear];
+        }
+        rear--;
+    }
+
+    return canDequeue;
 }
 
 // Returns the front of the queue
@@ -48,19 +69,19 @@ T FixedQueue<T>::peek() const
 template <class T>
 bool FixedQueue<T>::isFull() const
 {
-    return false;
+    return rear == maxSize;
 }
 
 // Removes all items from the queue
 template <class T>
 void FixedQueue<T>::clear()
 {
-
+    rear = 0;
 }
 
 // Returns the number of items in the queue
 template <class T>
 int FixedQueue<T>::length() const
 {
-    return -1;
+    return rear;
 }
