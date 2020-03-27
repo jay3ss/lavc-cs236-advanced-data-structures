2f3587da12f779ae2fae56d8017ddd88f3363040
jay ess <mentoce@gmail.com>
2020-03-27 13:33:22 -0700

    Create method stubs for bst

---
+++ assignments/assignment05/BSTree.cpp
@@ -0,0 +1,41 @@
+#include "BSTree.h"
+
+BST::BST()
+{
+    root = nullptr;
+}
+
+int BST::height() const
+{
+    return -1;
+}
+
+int BST::height(BTreeNode* tree)
+{
+    return -1;
+}
+
+void BST::insert(double x)
+{
+
+}
+
+void BST::inorder(vector<double>& vect)
+{
+
+}
+
+void BST::inorder(vector<double>& tlist, BTreeNode* tree)
+{
+
+}
+
+int BST::leafCounter() const
+{
+    return -1;
+}
+
+int BST::leafCounter(BTreeNode* tree) const
+{
+    return -1;
+}
