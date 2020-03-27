e5cd81bd83cdb29942625992404e593762325032
jay ess <mentoce@gmail.com>
2020-03-27 14:29:49 -0700

    Add test for checking the height of a new tree

--- assignments/assignment05/tests.cpp
+++ assignments/assignment05/tests.cpp
@@ -9,5 +9,8 @@ int main()
     BST bst;
 
     assert(bst.isEmpty() && "FAILED: New tree isn't empty\n");
-    cout << "A new tree should be empty: PASSED\n\n";
+    cout << "A new tree should be empty: PASSED\n";
+
+    assert(bst.height() == 0 && "FAILED: New tree height isn't 0\n");
+    cout << "A new tree's height is zero: PASSED\n";
 }
