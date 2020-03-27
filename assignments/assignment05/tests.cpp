03b8ea92ca8b23a8c1720f38db072fbcc6664ad1
jay ess <mentoce@gmail.com>
2020-03-27 15:05:40 -0700

    Add tests for insertion, getting an entry, height of tree w/height = 1

--- assignments/assignment05/tests.cpp
+++ assignments/assignment05/tests.cpp
@@ -13,4 +13,14 @@ int main()
 
     assert(bst.height() == 0 && "FAILED: New tree height isn't 0\n");
     cout << "A new tree's height is zero: PASSED\n";
+
+    double entry = 1.37;
+    assert(bst.insert(entry) && "FAILED: Should be able to add an entry\n");
+    cout << "Should be able to add a new entry to the empty tree: PASSED\n";
+
+    assert(bst.entry(entry) == entry && "FAILED: Should be able to get the item\n");
+    cout << "SHould be able to get an item from the tree: PASSED\n";
+
+    assert(bst.height() == 1 && "FAILED: The height of a tree with one entry should be 1\n");
+    cout << "The height of a tree with one entry should be one: PASSED\n";
 }
