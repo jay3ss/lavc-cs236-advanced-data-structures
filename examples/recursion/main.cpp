04a10e3544812c230cd6f392b7bd578a7112035d
jay ess <mentoce@gmail.com>
2020-03-31 14:21:12 -0700

    Use factorial functions

--- examples/recursion/main.cpp
+++ examples/recursion/main.cpp
@@ -3,5 +3,8 @@
 
 int main()
 {
-
+    std::cout << "Recursive factorial(6) = " << recursive::factorial(6)
+              << "\n";
+    std::cout << "Iterative factorial(6) = " << iterative::factorial(6)
+              << "\n";
 }
