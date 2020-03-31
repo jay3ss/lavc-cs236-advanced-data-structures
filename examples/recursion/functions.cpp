8802944769b70a2fd1327da9c1a7f37860d9c60b
jay ess <mentoce@gmail.com>
2020-03-31 14:21:22 -0700

    Implement factorial functions

--- examples/recursion/functions.cpp
+++ examples/recursion/functions.cpp
@@ -2,18 +2,42 @@
 
 namespace recursive
 {
-template <class T>
-T factorial(const T &number)
-{
 
+int factorial(const int number)
+{
+    // number must not be negative
+    if (number < 0)
+    {
+        throw InvalidInput();
+    }
+
+    if (number == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return number * factorial(number - 1);
+    }
 }
 };
 
 namespace iterative
 {
-template <class T>
-T factorial(const T &number)
-{
 
+int factorial(const int number)
+{
+    if (number < 0)
+    {
+        throw InvalidInput();
+    }
+
+    int fact = 1;
+    for (int n = number; n > 1; n--)
+    {
+        fact = fact * n;
+    }
+
+    return fact;
 }
 };
