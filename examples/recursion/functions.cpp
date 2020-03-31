ef9d33ba7de8ad929d0c4c1524e5a15c7e4b640b
jay ess <mentoce@gmail.com>
2020-03-31 13:56:41 -0700

    Start work on examples from recursion lecture

---
+++ examples/recursion/functions.cpp
@@ -0,0 +1,19 @@
+#include "functions.h"
+
+namespace recursive
+{
+template <class T>
+T factorial(const T &number)
+{
+
+}
+};
+
+namespace iterative
+{
+template <class T>
+T factorial(const T &number)
+{
+
+}
+};
---
+++ examples/recursion/functions.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+namespace recursive
+{
+template <class T>
+T factorial(const T& number);
+};
+
+
+namespace iterative
+{
+template <class T>
+T factorial(const T& number);
+};
+
+#endif // FUNCTIONS_H
