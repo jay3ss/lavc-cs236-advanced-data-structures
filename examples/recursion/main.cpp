375e068835a3f020ac39832869faf3160df587ff
jay ess <mentoce@gmail.com>
2020-03-31 15:44:24 -0700

    Use recursive binary search function

--- examples/recursion/main.cpp
+++ examples/recursion/main.cpp
@@ -14,4 +14,24 @@ int main()
               << "\n";
 
     std::cout << std::endl;
+
+    const int NUM_ELEMENTS = 100;
+    int array[NUM_ELEMENTS];
+
+    for (int i = 0; i < NUM_ELEMENTS; i++)
+    {
+        array[i] = i;
+    }
+
+    std::cout << "Recursive binarySearch(array, 17, 0, 100) = "
+              << recursive::binarySearch(array, 17, 0, NUM_ELEMENTS)
+              << "\n";
+
+    std::cout << "Recursive binarySearch(array, -50, 0, 100) = "
+              << recursive::binarySearch(array, -50, 0, NUM_ELEMENTS)
+              << "\n";
+
+    std::cout << "Recursive binarySearch(array, 150, 0, 100) = "
+              << recursive::binarySearch(array, 150, 0, NUM_ELEMENTS)
+              << "\n";
 }
