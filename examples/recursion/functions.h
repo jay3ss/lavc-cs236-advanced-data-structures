9bfa28680107037029869b95ed95260f451fc9df
jay ess <mentoce@gmail.com>
2020-03-31 15:43:09 -0700

    Include implementation file and define recursive binary search algorithm

--- examples/recursion/functions.h
+++ examples/recursion/functions.h
@@ -5,6 +5,8 @@ namespace recursive
 {
 int factorial(const int number);
 int combinations(const int group, const int members);
+template <class T>
+int binarySearch(const T array[], const T value, const int start, const int end);
 };
 
 
@@ -16,4 +18,5 @@ int factorial(const int number);
 // Exception for invalid input
 class InvalidInput {};
 
+#include "functions.cpp"
 #endif // FUNCTIONS_H
