52f8a0a7b549a3e77c788039fa1790e6b1d7d17a
jay ess <mentoce@gmail.com>
2020-04-01 13:17:17 -0700

    Add helper functions to print array and swap variable contents

--- examples/recursion/functions.h
+++ examples/recursion/functions.h
@@ -7,6 +7,10 @@ int factorial(const int number);
 int combinations(const int group, const int members);
 template <class T>
 int binarySearch(const T array[], const T value, const int start, const int end);
+template <class T>
+void quickSort(T array[], const int first, const int last);
+template <class T>
+int split(T array[], const int first, const int last);
 };
 
 
@@ -17,6 +21,14 @@ int factorial(const int number);
 
 // Exception for invalid input
 class InvalidInput {};
+
+namespace helper
+{
+template <class T>
+void swap(T& a, T& b);
+template <class T>
+void printArray(const T array[], const int length);
+};
 
 #include "functions.cpp"
 #endif // FUNCTIONS_H
