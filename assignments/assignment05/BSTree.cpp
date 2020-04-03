ba35955f2421ba669bbc61842705f6e46b381258
jay ess <mentoce@gmail.com>
2020-04-02 21:38:00 -0700

    Implement method to remove items from bst, fix bug for inserting and searching for items in bst

--- assignments/assignment05/BSTree.cpp
+++ assignments/assignment05/BSTree.cpp
@@ -106,11 +106,11 @@ BTreeNode* BST::insert(BTreeNode*& tree, BTreeNode*& newNode)
     // 1. go to the left subtree
     // 2. go to the right subtree
     // 3. we found the insertion point
-    if (tree->value <= newNode->value)
+    if (tree->value >= newNode->value)
     {
         tree->left = insert(tree->left, newNode);
     }
-    else if (tree->value > newNode->value)
+    else if (tree->value < newNode->value)
     {
         tree->right = insert(tree->right, newNode);
     }
@@ -134,11 +134,11 @@ BTreeNode* BST::find(BTreeNode* tree, double val) const
     // 1. go to the left subtree
     // 2. go to the right subtree
     // 3. we found the value, return the pointer to it
-    if (tree->value < val)
+    if (tree->value > val)
     {
         return find(tree->left, val);
     }
-    else if (tree->value > val)
+    else if (tree->value < val)
     {
         return find(tree->right, val);
     }
@@ -161,22 +161,85 @@ BTreeNode* BST::remove(BTreeNode*& tree, const double val, bool& flag)
         return tree;
     }
 
-    // remove the only node from the tree
-    if (tree->value == val)
+    // 3 cases of where the value is
+    // 1. the value is in the left subtree
+    // 2. the value is in the right subtree
+    // 3. we found the value
+    if (tree->value > val) // 1. go left
+    {
+        tree->left = remove(tree->left, val, flag);
+    }
+    else if (tree->value < val) // 2. go right
+    {
+        tree->right = remove(tree->right, val, flag);
+    }
+    else // 3. found it
     {
         flag = true;
-        delete tree;
-        tree = nullptr;
-        return nullptr;
+        tree = removeNode(tree);
     }
+
+    return tree;
+}
+
+BTreeNode* BST::removeNode(BTreeNode*& tree)
+{
+    // 3 cases for removing the value from the node
+    // 1. node is a leaf
+    // 2. node has one child
+    // 3. node has two children
+    BTreeNode *toDelete = tree;
+    if (tree->left == nullptr) // 1. node is a leaf or 2. has a right child
+    {
+        tree = tree->right; // move the right subtree "up" one position
+    }
+    else if (tree->right == nullptr) // 2. has a left child
+    {
+        tree = tree->left; // move the left subtree "up" one position
+    }
+    else // 3. node has two children
+    {
+        // 1. find the logical predecessor (the maximum value in the
+        //    left subtree)
+        // 2. replace the current node's value with the logical
+        //    predecessor's
+        // 3. delete the logical predecessor
+        toDelete = getmax(tree->left); // logical predecessor
+        tree->value = toDelete->value;
+        tree->left = deletemax(tree->left);
+    }
+    delete toDelete;
+    toDelete = nullptr;
+
+    return tree;
 }
 
 BTreeNode* BST::getmax(BTreeNode*& tree)
 {
-    return nullptr;
+    // continue traversing the right subtree until the
+    // maxium value is found (can't continue)
+    if (tree->right == nullptr)
+    {
+        return tree;
+    }
+    else
+    {
+        return getmax(tree->right);
+    }
 }
 
-BTreeNode* deletemax(BTreeNode*& tree)
+BTreeNode* BST::deletemax(BTreeNode*& tree)
 {
-    return nullptr;
+    // 2 cases
+    // 1. found the max
+    // 2. haven't found it
+    if (tree->right == nullptr)
+    {
+        return tree->left;
+    }
+    else
+    {
+        tree->right = deletemax(tree->right);
+        return tree;
+    }
 }
