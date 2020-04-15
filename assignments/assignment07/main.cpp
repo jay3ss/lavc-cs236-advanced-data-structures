b6fc3f95f3143239a1b591481291c41c781b46c7
jay ess <mentoce@gmail.com>
2020-04-14 19:45:51 -0700

    Use given main program driver

--- assignments/assignment07/main.cpp
+++ assignments/assignment07/main.cpp
@@ -3,8 +3,22 @@
 
 using namespace std;
 
+template <class T>
+void heapSort(T list[], int arraySize)
+{
+
+}
+
 int main()
 {
-    Heap<int> heap;
+    const int SIZE = 9;
+    int list[] = { 1, 7, 3, 4, 9, 11, 3, 1, 2 };
+    heapSort(list, SIZE);
+    cout << "Sorted elements using heap:\n";
+    for (int i = 0; i < SIZE; i++)
+        cout << list[i] << " ";
 
+    cout << endl;
+
+    return 0;
 }
