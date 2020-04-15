01c44887eb7292ca1211917d731b2469697c60fb
jay ess <mentoce@gmail.com>
2020-04-14 20:50:45 -0700

    Add some more method definitions and dummy implmetations

--- assignments/assignment07/Heap.h
+++ assignments/assignment07/Heap.h
@@ -3,32 +3,70 @@
 
 #include <vector>
 
+enum class Heapify { MAX, MIN };
+
 template <class T>
 class Heap
 {
 public:
     Heap();
-    Heap(const T elements[], int arraySize);
+    Heap(const T elements[], const int arraySize);
     ~Heap();
 
-    // remove the root from the heap and maintain the max-heap property
-    T remove();
-
-    // insert element into the heap and maintain the max-heap property
+    // inserts element into the heap and maintain the max-heap property
     void add(const T& element);
 
-    // get the number of elements in the heap
+    // returns the maximum value in the heap
+    T max() const;
+
+    // removes the root from the heap and maintain the max-heap property
+    T remove();
+
+    // returns the number of elements in the heap
     int size() const;
 
 private:
-    std::vector<T> vect;
+    std::vector<T> tree;    // to hold the heap items
+
+    // produces a max-heap from an unordered array
+    void buildHeap(const T elements[], const int arraySize);
+
+    // corrects a single violation of the max-heap property in subtree at its root
+    void heapify(std::vector<T>& subtree, const int index);
+
+    // returns true if the node is a leaf, false if not
+    bool isLeaf(const int index) const;
+
+    // returns the index of the left child of the node at the given index
+    static int left(const int index);
+
+    // returns the index of the parent of the node at the given index
+    static int parent(const int index);
+
+    // returns the index of the right child of the node at the given index
+    static int right(const int index);
 };
 
 template <class T>
 Heap<T>::Heap() {}
 
 template <class T>
+Heap<T>::Heap(const T elements[], const int arraySize)
+{
+
+}
+
+template <class T>
 Heap<T>::~Heap() {}
+
+template <class T>
+void Heap<T>::add(const T& element)
+{
+    // put the element in the leaf that's furthest to the right (end of the
+    // vector) and then ensure that it doesn't violate the max-heap property
+    tree.push_back(element);
+    heapify(tree, 0);
+}
 
 template <class T>
 T Heap<T>::remove()
@@ -37,16 +75,15 @@ T Heap<T>::remove()
 }
 
 template <class T>
-void Heap<T>::add(const T& element)
+int Heap<T>::size() const
 {
-
+    return tree.size();
 }
 
 template <class T>
-int Heap<T>::size() const
+T Heap<T>::max() const
 {
-    return -1;
+    return tree[0];
 }
-
 
 #endif // HEAP_H
