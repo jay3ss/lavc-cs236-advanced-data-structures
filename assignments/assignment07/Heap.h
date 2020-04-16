8c4a90bd073b465f5bdea674cf86b3c470917952
jay ess <mentoce@gmail.com>
2020-04-15 19:05:36 -0700

    Commit to change a lot of stuff

--- assignments/assignment07/Heap.h
+++ assignments/assignment07/Heap.h
@@ -32,19 +32,27 @@ private:
     void buildHeap(const T elements[], const int arraySize);
 
     // corrects a single violation of the max-heap property in subtree at its root
-    void heapify(std::vector<T>& subtree, const int index);
+    void heapify(const int index);
+
+    // corrects a violation of the max-heap property caused by removing the root
+    void heapifyDown(int index);
+
+    // corrects a violation of the max-heap property caused by adding an element
+    void heapifyUp(int index);
 
     // returns true if the node is a leaf, false if not
     bool isLeaf(const int index) const;
 
     // returns the index of the left child of the node at the given index
-    static int left(const int index);
+    static int leftChild(const int index);
 
     // returns the index of the parent of the node at the given index
     static int parent(const int index);
 
     // returns the index of the right child of the node at the given index
-    static int right(const int index);
+    static int rightChild(const int index);
+
+    void swap(T& item1, T& item2);
 };
 
 template <class T>
@@ -62,10 +70,7 @@ Heap<T>::~Heap() {}
 template <class T>
 void Heap<T>::add(const T& element)
 {
-    // put the element in the leaf that's furthest to the right (end of the
-    // vector) and then ensure that it doesn't violate the max-heap property
-    tree.push_back(element);
-    heapify(tree, 0);
+
 }
 
 template <class T>
@@ -93,39 +98,71 @@ void Heap<T>::buildHeap(const T elements[], const int arraySize)
 
 }
 
-// corrects a single violation of the max-heap property in subtree at its root
+// produces a max-heap from an unordered vector
 template <class T>
-void Heap<T>::heapify(std::vector<T> &subtree, const int index)
+void Heap<T>::buildHeap(const T elements[], const int arraySize)
 {
 
+}
+
+// corrects a single violation of the max-heap property in subtree at its root
+template <class T>
+void Heap<T>::heapifyUp(const int index)
+{
+    int l = leftChild(index);
+    int r = rightChild(index);
+
+    // determine if the node's children are larger
+    // if the calculated children indices are within the tree's bounds and
+    int largest = ((l < size() && tree[l] > tree[index]) ? l : index);
+    largest = ((r < size() && tree[r] > tree[largest]) ? largest = r : largest);
+
+    // if the index of the largest isn't the current index, swap the values at
+    // keep on the two indices and keep on heapifying
+    if (largest != index)
+    {
+        swap(tree[index], tree[largest]);
+        heapify(largest);
+    }
 }
 
 // returns true if the node is a leaf, false if not
 template <class T>
 bool Heap<T>::isLeaf(const int index) const
 {
-
+    // A node is a leaf if the calculated index of its left child is greater
+    // than the size of the heap
+    return (leftChild(index) > size());
 }
 
 // returns the index of the left child of the node at the given index
 template <class T>
-int Heap<T>::left(const int index)
+int Heap<T>::leftChild(const int index)
 {
-    return 2 * index;
+    return (2 * index) + 1;
 }
 
 // returns the index of the parent of the node at the given index
 template <class T>
 int Heap<T>::parent(const int index)
 {
-    return index / 2;
+    return (index - 1) / 2;
 }
 
 // returns the index of the right child of the node at the given index
 template <class T>
-int Heap<T>::right(const int index)
+int Heap<T>::rightChild(const int index)
 {
-    return (2 * index) + 1;
+    return (2 * index) + 2;
+}
+
+// swap two items
+template <class T>
+void Heap<T>::swap(T& item1, T& item2)
+{
+    T temporary = item1;
+    item1 = item2;
+    item2 = temporary;
 }
 
 #endif // HEAP_H
--- assignments/assignment07/tests.cpp
+++ assignments/assignment07/tests.cpp
@@ -7,4 +7,9 @@ int main()
 {
     Heap<int> heap;
 
+    const int SIZE = 9;
+    int list[] = {1, 7, 3, 4, 9, 11, 3, 1, 2};
+
+    for (int i = 0; i < SIZE; i++)
+        heap.add(list[i]);
 }
