bbaf5aba2923cd1a5f2351744be7b439c9c14a95
jay ess <mentoce@gmail.com>
2020-04-19 16:57:20 -0700

    Add docs via comments

--- assignments/assignment07/Heap.h
+++ assignments/assignment07/Heap.h
@@ -7,61 +7,126 @@ template <class T>
 class Heap
 {
 public:
+    /** Constructor */
     Heap();
+
+    /** Constructor */
     Heap(const T elements[], const int arraySize);
+
+    /** Destructor */
     ~Heap();
 
-    // inserts element into the heap and maintain the max-heap property
+    /** inserts element into the heap and maintain the max-heap property
+     * @param element reference to the element to add to the heap
+     * @post the element will be added to the heap with the max-heap property
+     *      satisfied */
     void add(const T& element);
 
-    // corrects a violation of the max-heap property caused by removing the root
+    /** corrects a violation of the max-heap property of the given subheap
+     * @pre each of the children of the given subheap follow the max-heap
+     *      property
+     * @param subHeap a reference to the subheap to correct
+     * @param index the index of the root of the given subheap
+     * @post the subheap will have the max-heap property */
     void heapifyDown(std::vector<T>& subHeap, const int index);
 
+    /** corrects a violation of the max-heap property of the given subheap
+     * @pre each of the children of the given subheap follow the max-heap
+     *      property
+     * @param subHeap a reference to the subheap to correct
+     * @param index the index of the root of the given subheap
+     * @param heapSize the size of the given subheap
+     * @post the subheap will have the max-heap property */
     static void heapifyDown(T subHeap[], const int index, const int heapSize);
 
-    // returns the maximum value in the heap
+    /** returns the maximum value in the heap, if the heap is empty then an
+     *      EmptyHeap exception is thrown
+     * @pre the heap is not empty
+     * @returns the maximum value in the heap */
     T max() const;
 
-    // removes the root from the heap and maintain the max-heap property
+    /** removes the root from the heap and maintain the max-heap property, if
+     *      the heap is empty then an EmptyHeap exception is thrown
+     * @pre the heap is not empty
+     * @returns the maximum value in the heap
+     * @post the maximum value of the heap will be removed */
     T remove();
 
-    // returns the number of elements in the heap
+    /** returns the number of elements in the heap
+     * @returns the number of elements in the heap */
     int size() const;
 
-    // Exception class to indicate an empty heap
+    /** Exception class to indicate an empty heap */
     class EmptyHeap {};
 private:
-    std::vector<T> heap;    // to hold the heap items
+    std::vector<T> heap;    // to hold the heap elements
 
-    // corrects a violation of the max-heap property caused by adding an element
-    void heapifyUp(int index);
+    /** corrects a violation of the max-heap property caused by adding an
+     *      element
+     * @pre the subheap rooted at the index is not empty
+     * @param index the index of the root of the subheap
+     * @post the subheap rooted at the index will have the max-heap property */
+    void heapifyUp(const int index);
 
-    // returns true if the node is a leaf, false if not
+    /** returns true if the node is a leaf, false if not
+     * @param index the index of the node
+     * @returns true if the node is a leaf, false if not */
     bool isLeaf(const int index) const;
 
+    /** returns true if the node is a leaf, false if not
+     * @param index the index of the node
+     * @param heapSize the size of the array/heap
+     * @returns true if the node is a leaf, false if not */
     static bool isLeaf(const int index, const int heapSize);
 
-    // returns the index of the left child of the node at the given index
+    /** returns the index of the left child of the node at the given index
+     * @param index the index of the node
+     * @returns the index of the left child of the node if there is a left
+     *      child, returns the index if there isn't a left node */
     static int leftChild(const int index);
 
-    // returns the index of the parent of the node at the given index
+    /** returns the index of the parent of the node at the given index
+     * @param index the index of the node
+     * @returns the index of the parent of the node if the node is not the
+     *      root, if the node is the root then the root index is returned */
     static int parent(const int index);
 
-    // returns the index of the child node with the largest value
+    /** returns the index of the child node with the largest value
+     * @param index the index of the node
+     * @returns the index of the larger of the children nodes. if neither child
+     *      is larger than the given node, or if the node is a leaf, then the
+     *      node's index is returned */
     int largestChild(const int index);
 
+    /** returns the index of the child node with the largest value
+     * @param subHeap the subheap
+     * @param index the index of the node
+     * @param arraySize the size of the array/subheap
+     * @returns the index of the larger of the children nodes. if neither child
+     *      is larger than the given node, or if the node is a leaf, then the
+     *      node's index is returned */
     static int largestChild(T subHeap[], const int index, const int arraySize);
 
-    // returns the index of the right child of the node at the given index
+    /** returns the index of the right child of the node at the given index
+     * @param index the index of the node
+     * @returns the index of the right child of the node if there is a right
+     *      child, returns the index if there isn't a right node */
     static int rightChild(const int index);
 
-    // swaps the contents of two items
+    /** swaps the contents of two given items
+     * @param item1 the first item
+     * @param item2 the second item
+     * @post the contents of the two variables will be swapped */
     static void swap(T& item1, T& item2);
 };
 
+// Implementation
+
+// Constructor
 template <class T>
 Heap<T>::Heap() {}
 
+// Constructor
 template <class T>
 Heap<T>::Heap(const T elements[], const int arraySize)
 {
@@ -69,9 +134,11 @@ Heap<T>::Heap(const T elements[], const int arraySize)
         add(elements[i]);
 }
 
+// Destructor
 template <class T>
 Heap<T>::~Heap() {}
 
+// inserts element into the heap and maintain the max-heap property
 template <class T>
 void Heap<T>::add(const T& element)
 {
@@ -79,6 +146,7 @@ void Heap<T>::add(const T& element)
     heapifyUp(size() - 1);
 }
 
+// corrects a violation of the max-heap property of the given subheap
 template <class T>
 void Heap<T>::heapifyDown(std::vector<T>& subHeap, const int index)
 {
@@ -97,7 +165,7 @@ void Heap<T>::heapifyDown(std::vector<T>& subHeap, const int index)
     }
 }
 
-// static version
+// corrects a violation of the max-heap property of the given subheap
 template <class T>
 void Heap<T>::heapifyDown(T subHeap[], const int index, const int heapSize)
 {
@@ -116,6 +184,20 @@ void Heap<T>::heapifyDown(T subHeap[], const int index, const int heapSize)
     }
 }
 
+// returns the maximum value in the heap, if the heap is empty then an
+// EmptyHeap exception is thrown
+template <class T>
+T Heap<T>::max() const
+{
+    if (size() == 0)
+    {
+        throw EmptyHeap();
+    }
+    return heap[0];
+}
+
+// removes the root from the heap and maintain the max-heap property, if
+// the heap is empty then an EmptyHeap exception is thrown
 template <class T>
 T Heap<T>::remove()
 {
@@ -137,23 +219,14 @@ T Heap<T>::remove()
     return first;
 }
 
+// returns the number of elements in the heap
 template <class T>
 int Heap<T>::size() const
 {
     return heap.size();
 }
 
-template <class T>
-T Heap<T>::max() const
-{
-    if (size() == 0)
-    {
-        throw EmptyHeap();
-    }
-    return heap[0];
-}
-
-// corrects a single violation of the max-heap property in subtree at its root
+// corrects a violation of the max-heap property caused by adding an element
 template <class T>
 void Heap<T>::heapifyUp(const int index)
 {
@@ -166,6 +239,7 @@ void Heap<T>::heapifyUp(const int index)
         swap(heap[index], heap[parentIndex]);
     }
 
+    // if we're not at the root node, then keep heapifying up
     if (parentIndex != 0)
     {
         heapifyUp(parentIndex);
@@ -202,6 +276,7 @@ int Heap<T>::largestChild(const int index)
     return largest;
 }
 
+// returns the index of the child node with the largest value
 template <class T>
 int Heap<T>::largestChild(T subHeap[], const int index, const int heapSize)
 {
@@ -235,7 +310,7 @@ int Heap<T>::rightChild(const int index)
     return (2 * index) + 2;
 }
 
-// swap two items
+// swaps the contents of two given items
 template <class T>
 void Heap<T>::swap(T& item1, T& item2)
 {
