233017a81df255b469393fee59bfda5272fbca32
jay ess <mentoce@gmail.com>
2020-05-07 18:17:32 -0700

    Define & implement insertion sort

--- assignments/assignment08/Sorters.cpp
+++ assignments/assignment08/Sorters.cpp
@@ -1,1 +1,26 @@
 #include "Sorters.h"
+
+template <class T>
+void insertion(T array[], const int length)
+{
+    for (int i = 1; i < length; i++)
+    {
+        int location = i;
+        T item = array[location];
+
+        while ((location > 0) && (array[location - 1] > item))
+        {
+            swap(array[location - 1], array[location]);
+            location--;
+        }
+        array[location] = item;
+    }
+}
+
+template <class T>
+void swap(T &item1, T &item2)
+{
+    T temp = item1;
+    item1 = item2;
+    item2 = temp;
+}
--- assignments/assignment08/Sorters.h
+++ assignments/assignment08/Sorters.h
@@ -1,6 +1,11 @@
 #ifndef SORTERS_H
 #define SORTERS_H
 
+template <class T>
+void insertion(T array[], const int length);
+
+template <class T>
+void swap(T &item1, T &item2);
 
 #include "Sorters.cpp"
 #endif // SORTERS
