19efcc02325375c2e7f1d0e7631e7bb810fb47d9
jay ess <mentoce@gmail.com>
2020-05-07 18:21:09 -0700

    Initial definition of pivot methods for quicksort

--- assignments/assignment08/Sorters.h
+++ assignments/assignment08/Sorters.h
@@ -5,6 +5,12 @@ template <class T>
 void insertion(T array[], const int length);
 
 template <class T>
+T pivotMedian(const T array[], const int first, const int last);
+
+template <class T>
+T pivotMiddle(const T array[], const int first, const int last);
+
+template <class T>
 void swap(T &item1, T &item2);
 
 #include "Sorters.cpp"
