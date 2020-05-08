ca03826cd922c7fd017b6fa70469bd4b1195063b
jay ess <mentoce@gmail.com>
2020-05-08 00:39:34 -0700

    Rename pivot* functions to partition* methods & remove const keyword from param list

--- assignments/assignment08/Sorters.h
+++ assignments/assignment08/Sorters.h
@@ -7,16 +7,16 @@ template <class T>
 void insertion(T array[], const int length);
 
 template <class T>
-void quickSort(T array[], const int first, const int last, Pivot = Pivot::MEDIAN);
+void quickSort(T array[], const int first, const int last, Pivot pivot = Pivot::MEDIAN);
 
 template <class T>
-void quickInsertionSort(T array[], const int first, const int last, Pivot = Pivot::MEDIAN);
+void quickInsertionSort(T array[], const int first, const int last, Pivot pivot = Pivot::MEDIAN);
 
 template <class T>
-T pivotMedian(const T array[], const int first, const int last);
+T partitionMedian(T array[], const int first, const int last);
 
 template <class T>
-T pivotMiddle(const T array[], const int first, const int last);
+T partitionMiddle(T array[], const int first, const int last);
 
 template <class T>
 void swap(T &item1, T &item2);
