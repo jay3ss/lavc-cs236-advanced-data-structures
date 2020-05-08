fb5b036a23b1a74a45368362b7afb9bbb4eff517
jay ess <mentoce@gmail.com>
2020-05-07 19:22:46 -0700

    Initial definition of quicksort functions & enum

--- assignments/assignment08/Sorters.h
+++ assignments/assignment08/Sorters.h
@@ -1,8 +1,16 @@
 #ifndef SORTERS_H
 #define SORTERS_H
 
+enum class Pivot {MEDIAN, MIDDLE};
+
 template <class T>
 void insertion(T array[], const int length);
+
+template <class T>
+void quickSort(T array[], const int first, const int last, Pivot = Pivot::MEDIAN);
+
+template <class T>
+void quickInsertionSort(T array[], const int first, const int last, Pivot = Pivot::MEDIAN);
 
 template <class T>
 T pivotMedian(const T array[], const int first, const int last);
